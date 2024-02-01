711c930195d8ff863e8d61d8547bb9df8b8a73ed
Thouseef <thousithouseef731@gmail.com>
2024-02-02 02:50:32 +0530

    file storage added

--- scholartrail/settings.py
+++ scholartrail/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 from django.contrib.messages import constants as messages
 
